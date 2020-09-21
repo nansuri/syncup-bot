289ab7b02bb1505b4e8854f563e398bbdad5dbf1
Riza P Nansuri <riza.nansuri@dana.id>
2020-09-21 19:50:55 +0700

    springboot init

---
+++ src/main/java/com/justnansuri/syncupbot/SyncupBotApplication.java
@@ -0,0 +1,13 @@
+package com.justnansuri.syncupbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncupBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncupBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/justnansuri/syncupbot/SyncupBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justnansuri.syncupbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncupBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
